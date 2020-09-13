aad5510643e3a561b17a93e24ddf5c88e1c49df3
labPC <freedom19970821@gmail.com>
2020-09-13 10:37:37 +0800

    4_31 Get the Player Controller with C++

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "TankAIController.h"
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto pc_t = GetWorld()->GetFirstPlayerController();
+	if (!pc_t) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController GetPlayerTank: can't get player controller!"));
+		return nullptr;
+	}
+
+	auto tpc_t = Cast<ATankPlayerController>(pc_t);
+	if (!tpc_t) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController GetPlayerTank: controller cast failed!"));
+		return nullptr;
+	}
+
+	return tpc_t->GetControlledTank();
+}
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto aiTank_t = GetControlledTank();
+	if (!aiTank_t) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController BeginPlay: can't get controlled tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController BeginPlay: controlled tank is %s"), *aiTank_t->GetName());
+	}
+
+	auto playerTank_t = GetPlayerTank();
+	if (!playerTank_t) {
+		UE_LOG(LogTemp, Error, TEXT("TankAIController BeginPlay: can't get player tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankAIController BeginPlay: player tank is %s"), *playerTank_t->GetName());
+	}
+}
---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankPlayerController.h"
+
+ATank* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto tank_t = GetControlledTank();
+	if (!tank_t) {
+		UE_LOG(LogTemp, Error, TEXT("TankPlayerController BeginPlay: can't get controlled tank!"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("TankPlayerController BeginPlay: %s"), *tank_t->GetName());
+	}
+
+}
