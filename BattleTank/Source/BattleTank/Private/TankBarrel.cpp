6288f83b5a5c47f362ea0c4694828c5c172b594d
labPC <freedom19970821@gmail.com>
2020-09-27 19:10:31 +0800

    4_53 Setting Up Projectile (ready for upgrade to 4.12)

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -8,7 +8,7 @@ void UTankBarrel::Elevate(float RelativeSpeed)
 	auto RelativeSpeedClamp = FMath::Clamp<float>(RelativeSpeed, -1.0, 1.0);
 
 	auto ElevationDegrees = RelativeSpeedClamp * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	auto NewElevationDegrees = RelativeRotation.Pitch + ElevationDegrees;
+	auto NewElevationDegrees = GetRelativeRotation().Pitch + ElevationDegrees;
 
 	auto NewElevationDegreesClamp = FMath::Clamp<float>(NewElevationDegrees, MinElevationDegrees, MaxElevationDegrees);
 	SetRelativeRotation(FRotator(NewElevationDegreesClamp, 0, 0));
--- BattleTank/Source/BattleTank/Private/TankTurret.cpp
+++ BattleTank/Source/BattleTank/Private/TankTurret.cpp
@@ -8,7 +8,7 @@ void UTankTurret::Rotate(float RelativeSpeed)
 	auto RelativeSpeedClamp = FMath::Clamp<float>(RelativeSpeed, -1.0, 1.0);
 
 	auto RotateDegrees = RelativeSpeedClamp * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
-	auto NewRotateDegrees = RelativeRotation.Yaw + RotateDegrees;
+	auto NewRotateDegrees = GetRelativeRotation().Yaw + RotateDegrees;
 
 	SetRelativeRotation(FRotator(0, NewRotateDegrees, 0));
 }
