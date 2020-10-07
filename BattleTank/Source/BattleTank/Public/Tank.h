777d0f1fa8356aaad5c71d659041b03661f0fe36
labPC <freedom19970821@gmail.com>
2020-10-07 22:09:12 +0800

    4_109 BlueprintPure and Health Bars

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -16,6 +16,9 @@ public:
 	// Sets default values for this pawn's properties
 	ATank();
 
+	UFUNCTION(BlueprintPure, Category = "State")
+	inline float GetHealthPercentage() const { return (float)CurrentHealth / (float)StartingHealth; }
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
